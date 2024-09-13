1c9637ca41054795de35ac30a8cffbbf5bb918aa
asafahmaddev <shenwatson480@gmail.com>
2024-09-13 10:10:29 +0500

    if satement in javacript

---
+++ lec10_assignment_operator_javascript/lec10.js
@@ -0,0 +1,14 @@
+//assignment Operators, 
+// 1- simple, =, a=b,a=a+b
+// 2-compound
+
+//compound operator
+
+var a = 8;
+var b = 5
+//a = a + b; //real from/simple 
+//a += b; //compound operator
+//a -= b; //compound operator
+a *= b; //compound operator
+//a /= b;   //compound operator
+console.log("result", a)
